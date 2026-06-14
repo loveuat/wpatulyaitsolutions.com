862154b4199fb9cd90b212768839dbaae09ef6ef
loveuat <atulbramhe@gmail.com>
2026-06-15 01:22:00 +0530

    Added code for rest api

--- wp-content/themes/ais-next-generation/functions.php
+++ wp-content/themes/ais-next-generation/functions.php
@@ -3137,24 +3137,7 @@ add_action('rest_api_init', function () {
 function ais_save_lead($request)
 {
     $data = $request->get_json_params();
-    error_log(print_r($data, true));
-    $form_id = 7;
-
-// $entry_meta = array(
-//     array(
-//         'name'  => 'text-1',
-//         'value' => 'Atul'
-//     ),
-//     array(
-//         'name'  => 'email-1',
-//         'value' => 'atul@example.com'
-//     )
-// );
-
-// $entry_id = Forminator_API::add_form_entry(
-//     $form_id,
-//     $entry_meta
-// );
+   
 
 
 
@@ -3167,7 +3150,24 @@ function ais_save_lead($request)
             ['status' => 400]
         );
     }
+     error_log(print_r($data, true));
+    $form_id = $data['form_id'];
 
+	// $entry_meta = array(
+    // array(
+    //     'name'  => 'text-1',
+    //     'value' => 'Atul'
+    // ),
+    // array(
+    //     'name'  => 'email-1',
+    //     'value' => 'atul@example.com'
+    // )
+	// );
+
+$entry_id = Forminator_API::add_form_entry(
+    $form_id,
+   // $entry_meta
+);
     return [
         'success' => true,
         'data' => $data
